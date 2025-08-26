26e3f45dfb888b06160d18f596b6cc2e55325800
Mostafizur <mostafizzfuad@gmail.com>
2025-08-26 15:09:07 +0600

    Explore Styling in React

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 // App.jsx
-import UserList from "./UserList";
+import styled from "styled-components";
+
+const Button = styled.button`
+	background: purple;
+	color: white;
+	padding: 10px 20px;
+	border-radius: 10px;
+	border: none;
+	cursor: pointer;
+
+	&:hover {
+		background: darkblue;
+	}
+`;
+
+
 
 export default function App() {
-	return <UserList />;
+	return <Button>Click Me</Button>;
 }
+
---
+++ src/InlineStyle.jsx
@@ -0,0 +1,7 @@
+export default function InlineStyle() {
+	return (
+		<h2 style={{ color: "blue", fontSize: "24px" }}>
+			Hello, Inline Style!
+		</h2>
+	);
+}
